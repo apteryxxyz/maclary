90d226fb7cfe73daf98005f7db346e407760f752
Apteryx <apteryx.xyz@gmail.com>
2023-03-24 22:30:00 +1300

    fix(Request): add missing user agent

--- packages/lists/lib/utilities/Request.ts
+++ packages/lists/lib/utilities/Request.ts
@@ -1,6 +1,16 @@
+import process from 'node:process';
 import { URL } from 'node:url';
 
+const UserAgent =
+    `@maclary/lists@[VI]{{inject}}[/VI], node.js/${process.version}` as `@maclary/lists@${string}, node.js/${string}`;
+
 export class Request extends null {
+    /**
+     * Perform a HTTP request.
+     * @param method Request method.
+     * @param init Request URL.
+     * @param options Request options.
+     */
     public static async perform<T>(
         method: string,
         init: string | URL,
@@ -14,7 +24,7 @@ export class Request extends null {
         for (const [key, value] of Object.entries(options.headers ?? {}))
             if (value) headers.set(key, String(value ?? ''));
 
-        headers.append('User-Agent', 'UserAgent');
+        headers.append('User-Agent', UserAgent);
         headers.append('Content-Type', 'application/json');
 
         const body = options.body ? JSON.stringify(options.body) : undefined;
@@ -24,13 +34,19 @@ export class Request extends null {
 }
 
 export namespace Request {
+    /** Request options. */
     export interface Options {
+        /** Request headers. */
         headers?: Record<string, unknown>;
+        /** Request URL query. */
         query?: Record<string, string | number | boolean>;
+        /** Request body. */
         body?: Record<string, unknown>;
     }
 
+    /** Additional request options. */
     export interface AdditionalOptions {
+        /** Whether the request requires an API token. */
         requiresApiToken?: boolean;
     }
 }
